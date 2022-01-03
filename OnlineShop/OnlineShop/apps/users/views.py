f3f1c7042deda30b68d576a57fad37355f29df61
lp1506947671 <1506946771@qq.com>
2022-01-03 11:37:30 +0800

    [类型]:需求开发 [描述]:用户给注册后端逻辑,状态保持,用户名重复注册,手机号重复注册 [提交者]:Jason

--- OnlineShop/OnlineShop/apps/users/views.py
+++ OnlineShop/OnlineShop/apps/users/views.py
@@ -1,7 +1,14 @@
+# Create your views here.
+import re
+
+from django import http
+from django.contrib.auth import login
+from django.views import View
+from sqlite3 import DatabaseError
 from django.shortcuts import render
 
-# Create your views here.
-from django.views import View
+from OnlineShop.utils.response_code import RETCODE
+from users.models import User
 
 
 class RegisterView(View):
@@ -9,3 +16,65 @@ class RegisterView(View):
 
     def get(self, request):
         return render(request, 'register.html')
+
+    def post(self, request):
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        password2 = request.POST.get('password2')
+        mobile = request.POST.get('mobile')
+        allow = request.POST.get('allow')
+
+        # 判断参数是否齐全
+        if not all([username, password, password2, mobile, allow]):
+            return http.HttpResponseForbidden('缺少必传参数')
+        # 判断用户名是否是5-20个字符
+        if not re.match(r'^[a-zA-Z0-9_-]{5,20}$', username):
+            return http.HttpResponseForbidden('请输入5-20个字符的用户名')
+        # 判断密码是否是8-20个数字
+        if not re.match(r'^[0-9A-Za-z]{8,20}$', password):
+            return http.HttpResponseForbidden('请输入8-20位的密码')
+        # 判断两次密码是否一致
+        if password != password2:
+            return http.HttpResponseForbidden('两次输入的密码不一致')
+        # 判断手机号是否合法
+        if not re.match(r'^1[3-9]\d{9}$', mobile):
+            return http.HttpResponseForbidden('请输入正确的手机号码')
+        # 判断是否勾选用户协议
+        if allow != 'on':
+            return http.HttpResponseForbidden('请勾选用户协议')
+        # 保存注册数据
+        try:
+            user = User.objects.create_user(username=username, password=password, mobile=mobile)
+        except DatabaseError:
+            return render(request, 'register.html', {'register_errmsg': '注册失败'})
+        # 登入用户，实现状态保持
+        login(request, user)
+
+        # 响应注册结果
+        return http.HttpResponse('注册成功，重定向到首页')
+
+
+class UsernameCountView(View):
+    """判断用户名是否重复注册"""
+
+    def get(self, request, username):
+        """
+        :param request: 请求对象
+        :param username: 用户名
+        :return: JSON
+        """
+        count = User.objects.filter(username=username).count()
+        return http.JsonResponse({'code': RETCODE.OK, 'errmsg': 'OK', 'count': count})
+
+
+class MobileCountView(View):
+    """判断手机号是否重复注册"""
+
+    def get(self, request, mobile):
+        """
+        :param request: 请求对象
+        :param mobile: 手机号
+        :return: JSON
+        """
+        count = User.objects.filter(mobile=mobile).count()
+        return http.JsonResponse({'code': RETCODE.OK, 'errmsg': 'OK', 'count': count})
--- OnlineShop/OnlineShop/urls.py
+++ OnlineShop/OnlineShop/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, re_path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    re_path(r'^', include('users.urls', namespace='users')),
+    re_path(r'^', include('contents.urls', namespace='contents')),
+    re_path(r'^', include('users.urls', namespace='users'))
 ]
