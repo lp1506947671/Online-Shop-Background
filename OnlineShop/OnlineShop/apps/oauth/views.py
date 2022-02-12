60eb1e51a5bb8b1942525d5782a8c30fd068ab9a
lp1506947671 <1506946771@qq.com>
2022-02-12 18:52:16 +0800

    [类型]:需求开发 [描述]:将openid与用户账号进行绑定 [提交者]:Jason

--- OnlineShop/OnlineShop/apps/oauth/views.py
+++ OnlineShop/OnlineShop/apps/oauth/views.py
@@ -1,14 +1,19 @@
 import logging
+import re
+
 from django import http
 from django.contrib.auth import login
+from django.db import DatabaseError
 from django.shortcuts import render, redirect
 from django.views import View
 from QQLoginTool.QQtool import OAuthQQ
+from django_redis import get_redis_connection
 
 from OnlineShop.settings.common import config_qq
-from OnlineShop.utils.common import gen_access_token
+from OnlineShop.utils.common import gen_access_token, check_access_token
 from OnlineShop.utils.response_code import RETCODE
 from oauth.models import OauthQQUser
+from users.models import User
 
 logger = logging.getLogger("django")
 
@@ -53,6 +58,65 @@ class QQAuthUserView(View):
             response.set_cookie("username", oauth_user.user.username, max_age=3600 * 24 * 15)
 
             return response
+
+    def post(self, request):
+        """美多商城用户绑定到openid"""
+        # 接收参数
+        mobile = request.POST.get('mobile')
+        pwd = request.POST.get('password')
+        sms_code_client = request.POST.get('sms_code')
+        access_token = request.POST.get('access_token')
+
+        # 校验参数
+        # 判断参数是否齐全
+        if not all([mobile, pwd, sms_code_client]):
+            return http.HttpResponseForbidden('缺少必传参数')
+        # 判断手机号是否合法
+        if not re.match(r'^1[3-9]\d{9}$', mobile):
+            return http.HttpResponseForbidden('请输入正确的手机号码')
+        # 判断密码是否合格
+        if not re.match(r'^[0-9A-Za-z]{8,20}$', pwd):
+            return http.HttpResponseForbidden('请输入8-20位的密码')
+        # 判断短信验证码是否一致
+        redis_conn = get_redis_connection('verify_code')
+        sms_code_server = redis_conn.get('sms_%s' % mobile)
+        if sms_code_server is None:
+            return render(request, 'oauth_callback.html', {'sms_code_errmsg': '无效的短信验证码'})
+        if sms_code_client != sms_code_server.decode():
+            return render(request, 'oauth_callback.html', {'sms_code_errmsg': '输入短信验证码有误'})
+        # 判断openid是否有效：错误提示放在sms_code_errmsg位置
+        openid = check_access_token(access_token)
+        if not openid:
+            return render(request, 'oauth_callback.html', {'openid_errmsg': '无效的openid'})
+
+        # 保存注册数据
+        try:
+            user = User.objects.get(mobile=mobile)
+        except User.DoesNotExist:
+            # 用户不存在,新建用户
+            user = User.objects.create_user(username=mobile, password=pwd, mobile=mobile)
+        else:
+            # 如果用户存在，检查用户密码
+            if not user.check_password(pwd):
+                return render(request, 'oauth_callback.html', {'account_errmsg': '用户名或密码错误'})
+
+        # 将用户绑定openid
+        try:
+            OauthQQUser.objects.create(openid=openid, user=user)
+        except DatabaseError:
+            return render(request, 'oauth_callback.html', {'qq_login_errmsg': 'QQ登录失败'})
+
+        # 实现状态保持
+        login(request, user)
+
+        # 响应绑定结果
+        next = request.GET.get('state')
+        response = redirect(next)
+
+        # 登录时用户名写入到cookie，有效期15天
+        response.set_cookie('username', user.username, max_age=3600 * 24 * 15)
+
+        return response
 
 
 class QQAuthURLView(View):
