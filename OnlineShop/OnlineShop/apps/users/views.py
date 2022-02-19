691c2e2c39fe7a88578d15425c0fc79609d5efc9
lp1506947671 <1506946771@qq.com>
2022-02-19 20:49:51 +0800

    [类型]:需求开发 [描述]:修改地址标题 [提交者]:Jason

--- OnlineShop/OnlineShop/apps/users/views.py
+++ OnlineShop/OnlineShop/apps/users/views.py
@@ -452,3 +452,27 @@ class DefaultAddressView(LoginRequiredJSONMixin, View):
 
         # 响应设置默认地址结果
         return http.JsonResponse({'code': RETCODE.OK, 'errmsg': '设置默认地址成功'})
+
+
+class UpdateTitleAddressView(LoginRequiredJSONMixin, View):
+    """设置地址标题"""
+
+    def put(self, request, address_id):
+        """设置地址标题"""
+        # 接收参数：地址标题
+        json_dict = json.loads(request.body.decode())
+        title = json_dict.get('title')
+
+        try:
+            # 查询地址
+            address = Address.objects.get(id=address_id)
+
+            # 设置新的地址标题
+            address.title = title
+            address.save()
+        except Exception as e:
+            logger.error(e)
+            return http.JsonResponse({'code': RETCODE.DBERR, 'errmsg': '设置地址标题失败'})
+
+        # 4.响应删除地址结果
+        return http.JsonResponse({'code': RETCODE.OK, 'errmsg': '设置地址标题成功'})
