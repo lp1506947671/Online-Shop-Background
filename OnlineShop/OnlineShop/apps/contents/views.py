7291afeae599c5c7a649276c72eac20adea21860
lp1506947671 <1506946771@qq.com>
2022-03-09 08:14:23 +0800

    [类型]:需求开发 [描述]:展示商品分类2 [提交者]:Jason

--- OnlineShop/OnlineShop/apps/contents/views.py
+++ OnlineShop/OnlineShop/apps/contents/views.py
@@ -1,11 +1,9 @@
 # Create your views here.
-from django.http import HttpResponse
-from collections import OrderedDict
-
 from django.shortcuts import render
 from django.views import View
 
-from goods.models import GoodsChannel
+from contents.models import ContentCategory
+from utils.contents_utils import get_categories
 
 
 class IndexView(View):
@@ -14,28 +12,7 @@ class IndexView(View):
     def get(self, request):
         """提供首页广告界面"""
         # 查询商品频道和分类
-        categories = OrderedDict()
-        channels = GoodsChannel.objects.order_by('group_id', 'sequence')
-        for channel in channels:
-            group_id = channel.group_id  # 当前组
-
-            if group_id not in categories:
-                categories[group_id] = {'channels': [], 'sub_cats': []}
-
-            cat1 = channel.category  # 当前频道的类别
-
-            # 追加当前频道
-            categories[group_id]['channels'].append({
-                'id': cat1.id,
-                'name': cat1.name,
-                'url': channel.url
-            })
-            # 构建当前类别的子类别
-            for cat2 in cat1.subs.all():
-                cat2.sub_cats = []
-                for cat3 in cat2.subs.all():
-                    cat2.sub_cats.append(cat3)
-                categories[group_id]['sub_cats'].append(cat2)
+        categories = get_categories()
 
         # 渲染模板的上下文
         context = {
