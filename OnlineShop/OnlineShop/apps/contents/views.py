e511f54d3a2299ecb8c2158bec93a6161e8a316c
lp1506947671 <1506946771@qq.com>
2022-03-09 08:25:02 +0800

    [类型]:需求开发 [描述]:展示首页商品广告_前后台逻辑 [提交者]:Jason

--- OnlineShop/OnlineShop/apps/contents/views.py
+++ OnlineShop/OnlineShop/apps/contents/views.py
@@ -13,9 +13,15 @@ class IndexView(View):
         """提供首页广告界面"""
         # 查询商品频道和分类
         categories = get_categories()
+        # 广告数据
+        contents = {}
+        content_categories = ContentCategory.objects.all()
+        for cat in content_categories:
+            contents[cat.key] = cat.content_set.filter(status=True).order_by('sequence')
 
         # 渲染模板的上下文
         context = {
             'categories': categories,
+            'contents': contents,
         }
         return render(request, 'index.html', context)
