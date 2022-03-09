3a9d4b7afd510fdfe6a8e6d144ca9ebce7b77a91
lp1506947671 <1506946771@qq.com>
2022-03-09 22:32:21 +0800

    [类型]:需求开发 [描述]:列表页分页和排序1 [提交人]:Jason

--- OnlineShop/OnlineShop/apps/goods/urls.py
+++ OnlineShop/OnlineShop/apps/goods/urls.py
@@ -8,6 +8,6 @@ from . import views
 app_name = "goods"
 urlpatterns = [
     # 商品列表
-    re_path(r'^list/(?P<category_id>\d+)/(?P<page_num>\d+)/$', views.ListView.as_view()),
+    re_path(r'^list/(?P<category_id>\d+)/(?P<page_num>\d+)/$', views.ListView.as_view(),name="list"),
 ]
 
--- OnlineShop/OnlineShop/apps/goods/views.py
+++ OnlineShop/OnlineShop/apps/goods/views.py
@@ -5,6 +5,7 @@ from django.views import View
 # Create your views here.
 from goods import models
 from goods.utils import get_breadcrumb
+from settings import constants
 from utils.contents_utils import get_categories
 
 
@@ -19,6 +20,30 @@ class ListView(View):
         except models.GoodsCategory.DoesNotExist:
             return http.HttpResponseNotFound('GoodsCategory does not exist')
 
+        # 接收sort参数：如果用户不传，就是默认的排序规则
+        sort = request.GET.get('sort', 'default')
+        # 按照排序规则查询该分类商品SKU信息
+        if sort == 'price':
+            # 按照价格由低到高
+            sort_field = 'price'
+        elif sort == 'hot':
+            # 按照销量由高到低
+            sort_field = '-sales'
+        else:
+            # 'price'和'sales'以外的所有排序方式都归为'default'
+            sort = 'default'
+            sort_field = 'create_time'
+        skus = models.SKU.objects.filter(category=category, is_launched=True).order_by(sort_field)
+        # 创建分页器：每页N条记录
+        paginator = Paginator(skus, constants.GOODS_LIST_LIMIT)
+        # 获取每页商品数据
+        try:
+            page_skus = paginator.page(page_num)
+        except EmptyPage:
+            # 如果page_num不正确，默认给用户404
+            return http.HttpResponseNotFound('empty page')
+        # 获取列表页总页数
+        total_page = paginator.num_pages
         # 查询商品频道分类
         categories = get_categories()
         # 查询面包屑导航
@@ -26,7 +51,13 @@ class ListView(View):
 
         # 渲染页面
         context = {
-            'categories': categories,
-            'breadcrumb': breadcrumb
+             'categories': categories,   # 频道分类
+            'breadcrumb': breadcrumb,   # 面包屑导航
+            'sort': sort,               # 排序字段
+            'category': category,       # 第三级分类
+            'page_skus': page_skus,     # 分页后数据
+            'total_page': total_page,   # 总页数
+            'page_num': page_num,       # 当前页码
+            "category_id":category_id
         }
         return render(request, 'list.html', context)
