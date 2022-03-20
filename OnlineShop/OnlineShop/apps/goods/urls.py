dbcfadb10f0db01c728aebe0ffa8f33a2d568017
lp1506947671 <xiaopawnye@163.com>
2022-03-20 20:10:02 +0800

    [类型]:需求开发 [描述]:展示详情页数据 [提交人]:Jason

--- OnlineShop/OnlineShop/apps/goods/urls.py
+++ OnlineShop/OnlineShop/apps/goods/urls.py
@@ -9,5 +9,6 @@ app_name = "goods"
 urlpatterns = [
     # 商品列表
     re_path(r'^list/(?P<category_id>\d+)/(?P<page_num>\d+)/$', views.ListView.as_view(), name="list"),
-    re_path(r'^hot/(?P<category_id>\d+)/$', views.HotGoodsView.as_view(), name="hot")
+    re_path(r'^hot/(?P<category_id>\d+)/$', views.HotGoodsView.as_view(), name="hot"),
+    re_path(r"^detail/(?P<sku_id>\d+)/$", views.DetailView.as_view(), name="detail")
 ]
