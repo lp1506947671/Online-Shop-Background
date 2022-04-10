bf25d20f57074413e74dab9cd7d44fe0534220be
lp1506947671 <xiaopawnye@163.com>
2022-04-10 15:17:35 +0800

    [类型]:需求开发 [描述]:修改购物车 [提交人]:Jason

--- OnlineShop/OnlineShop/apps/carts/views.py
+++ OnlineShop/OnlineShop/apps/carts/views.py
@@ -141,3 +141,89 @@ class CartsView(View):
                 "carts", cookie_cart_str, max_age=constants.CARTS_COOKIE_EXPIRES
             )
             return response
+
+    def put(self, request):
+        """修改购物车"""
+        # 接收和校验参数
+        json_dict = json.loads(request.body.decode())
+        sku_id = json_dict.get("sku_id")
+        count = json_dict.get("count")
+        selected = json_dict.get("selected", True)
+        # 判断参数是否齐全
+        if not all([sku_id, count]):
+            return http.HttpResponseForbidden("缺少必传参数")
+        # 判断sku_id是否存在
+        try:
+            sku = models.SKU.objects.get(id=sku_id)
+        except models.SKU.DoesNotExist:
+            return http.HttpResponseForbidden("商品sku_id不存在")
+        # 判断count是否为数字
+        try:
+            count = int(count)
+        except Exception:
+            return http.HttpResponseForbidden("参数count有误")
+        # 判断selected是否为bool值
+        if selected:
+            if not isinstance(selected, bool):
+                return http.HttpResponseForbidden("参数selected有误")
+
+        # 判断用户是否登录
+        user = request.user
+        if user.is_authenticated:
+            # 用户已登录，修改redis购物车
+            redis_conn = get_redis_connection("carts")
+            pl = redis_conn.pipeline()
+            # 因为接口设计为幂等的，直接覆盖
+            pl.hset("carts_%s" % user.id, sku_id, count)
+            # 是否选中
+            if selected:
+                pl.sadd("selected_%s" % user.id, sku_id)
+            else:
+                pl.srem("selected_%s" % user.id, sku_id)
+            pl.execute()
+
+            # 创建响应对象
+            cart_sku = {
+                "id": sku_id,
+                "count": count,
+                "selected": selected,
+                "name": sku.name,
+                "default_image_url": sku.default_image.url,
+                "price": sku.price,
+                "amount": sku.price * count,
+            }
+            return http.JsonResponse(
+                {"code": RETCODE.OK, "errmsg": "修改购物车成功", "cart_sku": cart_sku}
+            )
+        else:
+            # 用户未登录，修改cookie购物车
+            cart_str = request.COOKIES.get("carts")
+            if cart_str:
+                # 将cart_str转成bytes,再将bytes转成base64的bytes,最后将bytes转字典
+                # cart_dict = pickle.loads(base64.b64decode(cart_str.encode()))
+                cart_dict = s.loads(base64_decode(cart_str.encode()))
+            else:
+                cart_dict = {}
+            # 因为接口设计为幂等的，直接覆盖
+            cart_dict[sku_id] = {"count": count, "selected": selected}
+            # 将字典转成bytes,再将bytes转成base64的bytes,最后将bytes转字符串
+            # cookie_cart_str = base64.b64encode(pickle.dumps(cart_dict)).decode()
+            cookie_cart_str = base64_encode(s.dumps(cart_dict).encode()).decode()
+            # 创建响应对象
+            cart_sku = {
+                "id": sku_id,
+                "count": count,
+                "selected": selected,
+                "name": sku.name,
+                "default_image_url": sku.default_image.url,
+                "price": sku.price,
+                "amount": sku.price * count,
+            }
+            response = http.JsonResponse(
+                {"code": RETCODE.OK, "errmsg": "修改购物车成功", "cart_sku": cart_sku}
+            )
+            # 响应结果并将购物车数据写入到cookie
+            response.set_cookie(
+                "carts", cookie_cart_str, max_age=constants.CARTS_COOKIE_EXPIRES
+            )
+            return response
