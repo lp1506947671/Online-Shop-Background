0088895ee76e7eba0cdf130ac1f4f376f2fae08f
lp1506947671 <xiaopawnye@163.com>
2022-04-19 08:50:29 +0800

    [类型]:需求开发 [描述]:结算订单 [提交人]:Jason

--- OnlineShop/OnlineShop/apps/orders/views.py
+++ OnlineShop/OnlineShop/apps/orders/views.py
@@ -1,12 +1,59 @@
+from decimal import Decimal
+
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
 from django.views import View
 
-
 # Create your views here.
+from django_redis import get_redis_connection
+
+from goods.models import SKU
+from users.models import Address
+
+
 class OrderSettlementView(LoginRequiredMixin, View):
     """结算订单"""
 
     def get(self, request):
-        """提供订单结算页面"""
-        return render(request, "place_order.html")
+        # 获取登录用户
+        user = request.user
+        # 查询地址信息
+        try:
+            addresses = Address.objects.filter(user=request.user, is_deleted=False)
+        except Address.DoesNotExist:
+            # 如果地址为空，渲染模板时会判断，并跳转到地址编辑页面
+            addresses = None
+
+        # 从Redis购物车中查询出被勾选的商品信息
+        redis_conn = get_redis_connection("carts")
+        redis_cart = redis_conn.hgetall("carts_%s" % user.id)
+        cart_selected = redis_conn.smembers("selected_%s" % user.id)
+        cart = {}
+        for sku_id in cart_selected:
+            cart[int(sku_id)] = int(redis_cart[sku_id])
+
+        # 准备初始值
+        total_count = 0
+        total_amount = Decimal(0.00)
+        # 查询商品信息
+        skus = SKU.objects.filter(id__in=cart.keys())
+        for sku in skus:
+            sku.count = cart[sku.id]
+            sku.amount = sku.count * sku.price
+            # 计算总数量和总金额
+            total_count += sku.count
+            total_amount += sku.count * sku.price
+        # 补充运费
+        freight = Decimal("10.00")
+
+        # 渲染界面
+        context = {
+            "addresses": addresses,
+            "skus": skus,
+            "total_count": total_count,
+            "total_amount": total_amount,
+            "freight": freight,
+            "payment_amount": total_amount + freight,
+        }
+
+        return render(request, "place_order.html", context)
