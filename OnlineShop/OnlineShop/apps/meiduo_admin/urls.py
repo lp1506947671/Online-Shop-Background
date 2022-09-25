a23a1091114db2213fcc5d7cc751bc26879cbdf5
lp1506947671 <xiaopawnye@163.com>
2022-09-25 15:32:04 +0800

    [类型]:需求开发 [描述]:用户总数统计 [提交人]:Jason

--- OnlineShop/OnlineShop/apps/meiduo_admin/urls.py
+++ OnlineShop/OnlineShop/apps/meiduo_admin/urls.py
@@ -4,8 +4,11 @@
 from django.urls import re_path
 from rest_framework_jwt.views import obtain_jwt_token
 
+from .views import UserTotalCountView
+
 app_name = "meiduo_admin"
 urlpatterns = [
     # 登录
     re_path(r"authorizations", obtain_jwt_token),
+    re_path(r"statistical/total_count", UserTotalCountView.as_view()),
 ]
--- OnlineShop/OnlineShop/apps/meiduo_admin/views.py
+++ OnlineShop/OnlineShop/apps/meiduo_admin/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from datetime import date
+from rest_framework.response import Response
+
+from rest_framework.views import APIView
 
 # Create your views here.
+from users.models import User
+
+
+class UserTotalCountView(APIView):
+    def get(self, request):
+        now_date = date.today()
+        count = User.objects.all().count()
+        return Response({"count": count, "date": now_date})
