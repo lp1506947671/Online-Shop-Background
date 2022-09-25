bdd140d9e8f1af911b4e4f46f948d0481814cb9f
lp1506947671 <xiaopawnye@163.com>
2022-09-25 09:09:32 +0800

    [类型]:需求开发 [描述]:实现跨域访问支持3 [提交人]:Jason

--- OnlineShop/OnlineShop/settings/dev.py
+++ OnlineShop/OnlineShop/settings/dev.py
@@ -9,12 +9,10 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-import datetime
 import sys
 import os
 from pathlib import Path
 
-import rest_framework.authentication
 
 from . import common
 
