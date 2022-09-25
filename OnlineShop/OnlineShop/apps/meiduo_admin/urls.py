549e301eaf80be4656b8184bc954bcec4d4de005
lp1506947671 <xiaopawnye@163.com>
2022-09-25 15:38:10 +0800

    [类型]:需求开发 [描述]:日增用户统计 [提交人]:Jason

--- OnlineShop/OnlineShop/apps/meiduo_admin/urls.py
+++ OnlineShop/OnlineShop/apps/meiduo_admin/urls.py
@@ -4,11 +4,12 @@
 from django.urls import re_path
 from rest_framework_jwt.views import obtain_jwt_token
 
-from .views import UserTotalCountView
+from . import views
 
 app_name = "meiduo_admin"
 urlpatterns = [
     # 登录
     re_path(r"authorizations", obtain_jwt_token),
-    re_path(r"statistical/total_count", UserTotalCountView.as_view()),
+    re_path(r"statistical/total_count", views.UserTotalCountView.as_view()),
+    re_path(r"statistical/day_increment", views.UserDayCountView.as_view()),
 ]
--- OnlineShop/OnlineShop/apps/meiduo_admin/views.py
+++ OnlineShop/OnlineShop/apps/meiduo_admin/views.py
@@ -12,3 +12,12 @@ class UserTotalCountView(APIView):
         now_date = date.today()
         count = User.objects.all().count()
         return Response({"count": count, "date": now_date})
+
+
+class UserDayCountView(APIView):
+    def get(self, request):
+        # 获取当前日期
+        now_date = date.today()
+        # 获取当日注册用户数量 date_joined 记录创建账户时间
+        count = User.objects.filter(date_joined__gte=now_date).count()
+        return Response({"count": count, "date": now_date})
